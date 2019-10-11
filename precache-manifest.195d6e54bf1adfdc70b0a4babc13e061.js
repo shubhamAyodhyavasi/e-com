9363ea305aaba4a1ac7dfbe4b16c80ea20c7a52b
shubhamAyodhyavasi <shubhamAyodhyavasi@users.noreply.github.com>
2019-10-11 07:47:57 +0530

    Updates

---
+++ precache-manifest.195d6e54bf1adfdc70b0a4babc13e061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667ede0865ee7fd7d4c4a0abf051a29d",
+    "url": "/e-com/index.html"
+  },
+  {
+    "revision": "ca4f21807db8eb241586",
+    "url": "/e-com/static/css/main.03410456.chunk.css"
+  },
+  {
+    "revision": "56465a5e7f4ee542dedc",
+    "url": "/e-com/static/js/2.67762738.chunk.js"
+  },
+  {
+    "revision": "ca4f21807db8eb241586",
+    "url": "/e-com/static/js/main.dcf55bef.chunk.js"
+  },
+  {
+    "revision": "445677a373d318b20fa6",
+    "url": "/e-com/static/js/runtime-main.1d3b1d2d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/e-com/static/media/logo.25bf045c.svg"
+  }
+]);
